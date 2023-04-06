0e5d6880c6bfe8fc37ba2f385c630f5d71ee5983
aditya-routv <aditya@venus.io>
2023-04-06 18:59:54 +0530

    refactor: remove OZ contracts

--- contracts/Governance/AccessControlledV5.sol
+++ contracts/Governance/AccessControlledV5.sol
@@ -1,20 +1,16 @@
 // SPDX-License-Identifier: BSD-3-Clause
 pragma solidity 0.5.16;
 
-import "../Utils/InitializableV5.sol";
-import "../Utils/Ownable2StepUpgradeableV5.sol";
-
 import "./IAccessControlManagerV5.sol";
 
 /**
  * @title Venus Access Control Contract.
- * @dev The AccessControlledV5 contract is a wrapper around the OpenZeppelin AccessControl contract
- *      It provides a standardized way to control access to methods within the Venus Smart Contract Ecosystem.
- *      The contract allows the owner to set an AccessControlManager contract address.
- *      It can restrict method calls based on the sender's role and the method's signature.
+ * @dev This contract is helper between access control manager and actual contract
+ * This contract further inherited by other contract to integrate access controlled mechanism
+ * It provides initialise methods and verifying access methods
  */
 
-contract AccessControlledV5 is InitializableV5, Ownable2StepUpgradeableV5 {
+contract AccessControlledV5 {
     /// @notice Access control manager contract
     IAccessControlManagerV5 private _accessControlManager;
 
@@ -27,27 +23,6 @@ contract AccessControlledV5 is InitializableV5, Ownable2StepUpgradeableV5 {
 
     /// @notice Emitted when access control manager contract address is changed
     event NewAccessControlManager(address oldAccessControlManager, address newAccessControlManager);
-
-    /// @notice Thrown when the action is prohibited by AccessControlManager
-    // revert Unauthorized(address sender, address calledContract, string methodSignature);
-
-    function __AccessControlled_init(address accessControlManager_) internal onlyInitializing {
-        __Ownable2Step_init();
-        __AccessControlled_init_unchained(accessControlManager_);
-    }
-
-    function __AccessControlled_init_unchained(address accessControlManager_) internal onlyInitializing {
-        _setAccessControlManager(accessControlManager_);
-    }
-
-    /**
-     * @notice Sets the address of AccessControlManager
-     * @dev Admin function to set address of AccessControlManager
-     * @param accessControlManager_ The new address of the AccessControlManager
-     */
-    function setAccessControlManager(address accessControlManager_) external onlyOwner {
-        _setAccessControlManager(accessControlManager_);
-    }
 
     /**
      * @notice Returns the address of the access control manager contract
