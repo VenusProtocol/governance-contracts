9c4586d3073fbb5b32f61a7268eef7e36fdb6999
0xlucian <96285542+0xlucian@users.noreply.github.com>
2023-09-06 15:00:15 +0300

    Update deploy/001-access-control.ts
    Co-authored-by: Jesus Lanchas <jesus.lanchas@daureos.com>

--- deploy/001-access-control.ts
+++ deploy/001-access-control.ts
@@ -21,10 +21,10 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const networkName: string = hre.network.name;
   const adminAccount: string = acmAdminAccount[networkName];
 
-  console.log(`Granting DEDAULT_ADMIN_ROLE to ${adminAccount} for ${networkName} network`);
+  console.log(`Granting DEFAULT_ADMIN_ROLE to ${adminAccount} for ${networkName} network`);
   await acm.grantRole(acm.DEFAULT_ADMIN_ROLE(), acmAdminAccount[hre.network.name]);
 
-  console.log(`Renouncing DEDAULT_ADMIN_ROLE from deployer (${deployer}) for ${networkName} network`);
+  console.log(`Renouncing DEFAULT_ADMIN_ROLE from deployer (${deployer}) for ${networkName} network`);
   await acm.renounceRole(acm.DEFAULT_ADMIN_ROLE(), deployer);
 };
 
