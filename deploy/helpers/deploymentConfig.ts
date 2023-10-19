e11c035e960fde2b6954d5c0f908f8e615fdbc70
GitGuru7 <128375421+GitGuru7@users.noreply.github.com>
2023-10-19 16:57:03 +0530

    refactor: updated addresses

--- deploy/helpers/deploymentConfig.ts
+++ deploy/helpers/deploymentConfig.ts
@@ -6,14 +6,12 @@ const preconfiguredAddresses = {
     NormalTimelock: "0xce10739590001705F7FF231611ba4A48B2820327",
     FastTrackTimelock: "0x3CFf21b7AF8390fE68799D58727d3b4C25a83cb6",
     CriticalTimelock: "0x23B893a7C45a5Eb8c8C062b9F32d0D2e43eD286D",
-    GovernorBravo: "0x5573422A1a59385C247ec3a66B93B7C08eC2f8f2",
-    AccessControlManager: "0xdcfF60b259FbB051993Afa2e2Bbf05fC7Fae0c3e",
+    AccessControlManager: "0x45f8a08F534f34A97187626E05d4b6648Eeaa9AA",
   },
   bscmainnet: {
     NormalTimelock: "0x939bD8d64c0A9583A7Dcea9933f7b21697ab6396",
     FastTrackTimelock: "0x555ba73dB1b006F3f2C7dB7126d6e4343aDBce02",
     CriticalTimelock: "0x213c446ec11e45b15a6E29C1C1b402B8897f606d",
-    GovernorBravo: "0x2d56dC077072B53571b8252008C60e945108c75a",
     AccessControlManager: "0x4788629ABc6cFCA10F9f969efdEAa1cF70c23555",
   },
   sepolia: {
