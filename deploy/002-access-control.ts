2246a04ee72e3c3db668aa97280bbc97ae616397
Corey Rice <coreyarice@gmail.com>
2023-11-24 16:20:48 -0300

    refactor: support hardhat deployer as admin

--- deploy/002-access-control.ts
+++ deploy/002-access-control.ts
@@ -3,7 +3,7 @@ import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
 import { SUPPORTED_NETWORKS } from "../helpers/deploy/constants";
-import { acmAdminAccount } from "../helpers/deploy/deploymentUtils";
+import { getAcmAdminAccount } from "../helpers/deploy/deploymentUtils";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts } = hre;
@@ -18,12 +18,11 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   });
 
   const acm = await ethers.getContractAt("AccessControlManager", acmDeployment.address);
-
+  const networkName = hre.network.name as SUPPORTED_NETWORKS;
+  const adminAccount = await getAcmAdminAccount(networkName);
   if (hre.network.live) {
-    const networkName = hre.network.name as SUPPORTED_NETWORKS;
-    const adminAccount = acmAdminAccount[networkName];
     console.log(`Granting DEFAULT_ADMIN_ROLE to ${adminAccount} for ${networkName} network`);
-    await acm.grantRole(acm.DEFAULT_ADMIN_ROLE(), acmAdminAccount[networkName]);
+    await acm.grantRole(acm.DEFAULT_ADMIN_ROLE(), adminAccount);
 
     console.log(`Renouncing DEFAULT_ADMIN_ROLE from deployer (${deployer}) for ${hre.network.name} network`);
     await acm.renounceRole(acm.DEFAULT_ADMIN_ROLE(), deployer);
--- helpers/deploy/deploymentUtils.ts
+++ helpers/deploy/deploymentUtils.ts
@@ -1,14 +1,19 @@
 import bscMainnetDeployments from "@venusprotocol/venus-protocol/networks/mainnet.json";
 import bscTestnetDeployments from "@venusprotocol/venus-protocol/networks/testnet.json";
+import { getNamedAccounts } from "hardhat";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
 import { SUPPORTED_NETWORKS } from "./constants";
 
-export const acmAdminAccount: Record<SUPPORTED_NETWORKS, string> = {
-  bsctestnet: bscTestnetDeployments.Contracts.Timelock, // NORMAL TIMELOCK
-  bscmainnet: bscMainnetDeployments.Contracts.Timelock, // NORMAL TIMELOCK
-  sepolia: "0x94fa6078b6b8a26f0b6edffbe6501b22a10470fb", // SEPOLIA MULTISIG
-  ethereum: "0x285960C5B22fD66A736C7136967A3eB15e93CC67", // ETHEREUM MULTISIG
+export const getAcmAdminAccount = async (network: SUPPORTED_NETWORKS): string => {
+  const { deployer } = await getNamedAccounts();
+  return {
+    bsctestnet: bscTestnetDeployments.Contracts.Timelock, // NORMAL TIMELOCK
+    bscmainnet: bscMainnetDeployments.Contracts.Timelock, // NORMAL TIMELOCK
+    sepolia: "0x94fa6078b6b8a26f0b6edffbe6501b22a10470fb", // SEPOLIA MULTISIG
+    ethereum: "0x285960C5B22fD66A736C7136967A3eB15e93CC67", // ETHEREUM MULTISIG
+    hardhat: deployer,
+  }[network];
 };
 
 export const toAddress = async (addressOrAlias: string, hre: HardhatRuntimeEnvironment): Promise<string> => {
