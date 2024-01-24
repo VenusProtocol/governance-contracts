08d5fa944ac465c5d8a9c364b34bb0005932fc0b
GitGuru7 <128375421+GitGuru7@users.noreply.github.com>
2024-01-24 12:08:05 +0530

    fix: BOC-01

--- contracts/Cross-chain/BaseOmnichainControllerDest.sol
+++ contracts/Cross-chain/BaseOmnichainControllerDest.sol
@@ -2,7 +2,7 @@
 
 pragma solidity 0.8.13;
 
-import "@layerzerolabs/solidity-examples/contracts/lzApp/NonblockingLzApp.sol";
+import { NonblockingLzApp } from "@layerzerolabs/solidity-examples/contracts/lzApp/NonblockingLzApp.sol";
 import { Pausable } from "@openzeppelin/contracts/security/Pausable.sol";
 import { ensureNonzeroAddress } from "@venusprotocol/solidity-utilities/contracts/validators.sol";
 
