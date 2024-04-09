0d606f61627363d934c042d051c56ac5ddd0683d
GitGuru7 <128375421+GitGuru7@users.noreply.github.com>
2024-04-09 18:57:22 +0530

    refactor: hashing payload before call

--- contracts/Cross-chain/OmnichainGovernanceExecutor.sol
+++ contracts/Cross-chain/OmnichainGovernanceExecutor.sol
@@ -292,6 +292,7 @@ contract OmnichainGovernanceExecutor is ReentrancyGuard, BaseOmnichainController
         uint256 gasToStoreAndEmit = 30000; // enough gas to ensure we can store the payload and emit the event
 
         require(srcChainId_ == srcChainId, "OmnichainGovernanceExecutor::_blockingLzReceive: invalid source chain id");
+        bytes32 hashedPayload = keccak256(payload_);
 
         (bool success, bytes memory reason) = address(this).excessivelySafeCall(
             gasleft() - gasToStoreAndEmit,
@@ -300,7 +301,6 @@ contract OmnichainGovernanceExecutor is ReentrancyGuard, BaseOmnichainController
         );
         // try-catch all errors/exceptions
         if (!success) {
-            bytes32 hashedPayload = keccak256(payload_);
             failedMessages[srcChainId_][srcAddress_][nonce_] = hashedPayload;
             emit ReceivePayloadFailed(srcChainId_, srcAddress_, nonce_, reason); // Retrieve payload from the src side tx if needed to clear
         }
