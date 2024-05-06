0c45e361205ca3c556915db84007dc0f8048ee9f
GitGuru7 <128375421+GitGuru7@users.noreply.github.com>
2024-05-06 19:40:40 +0530

    feat: add default weights and its tests

--- contracts/Multi-Token-Governance/Vault/VaultAggregator/VaultAggregator.sol
+++ contracts/Multi-Token-Governance/Vault/VaultAggregator/VaultAggregator.sol
@@ -11,8 +11,14 @@ contract VaultAggregator is PausableUpgradeable, AccessControlledV8 {
     /// @notice Array containing instances of IVault contracts
     IVault[] public vaults;
 
+    /// @notice Default weights, used when weights are not provided, will be access controlled
+    uint8[] public defaultWeights;
+
     /// @notice Emit when vault is updated
-    event UpdateVault(IVault, bool);
+    event UpdateVault(IVault vault, bool isAdded);
+
+    ///@notice Emit when default weights of tokens updated
+    event DefaultWeightsUpdated(uint8[] oldWeights, uint8[] newWeights);
 
     /// @notice Error is thrown when the size of vaults increases by more than 7
     error VaultLimitExceed();
@@ -47,6 +53,13 @@ contract VaultAggregator is PausableUpgradeable, AccessControlledV8 {
         _unpause();
     }
 
+    /**
+     * @notice Update vaults
+     * @param vault Address of vault to be updated
+     * @param isAdded Bool, should be true to add vault
+     * @custom:event Emit UpdateVault with vault and its bool value
+     * @custom:access Controlled by Access Control Manager
+     */
     function updateVault(IVault vault, bool isAdded) external whenNotPaused {
         _checkAccessAllowed("updateVault(address,bool)");
         if (vaults.length >= 7) {
@@ -79,15 +92,37 @@ contract VaultAggregator is PausableUpgradeable, AccessControlledV8 {
         emit UpdateVault(vault, isAdded);
     }
 
+    /**
+     * @notice Update default weight of tokens
+     * @param weights New weights of tokens
+     * @custom:event Emit DefaultWeightsUpdated with old and new weights
+     * @custom:access Controlled by Access Controlled manager
+     */
+    function updateDefaultWeights(uint8[] calldata weights) external whenNotPaused {
+        _checkAccessAllowed("updateDefaultWeights(uint8[])");
+        emit DefaultWeightsUpdated(defaultWeights, weights);
+        defaultWeights = weights;
+    }
+
+    /**
+     * @notice Returns aggregated votes of given account based on given weights
+     * @param account Address of account whose votes need to be fetched
+     * @param blockNumberOrTimestamp Block number or Timestamp
+     * @param weights Weights of supported tokens
+     * @return votes Aggregated votes of user
+     */
     function getPriorVotes(
         address account,
         uint256 blockNumberOrTimestamp,
-        uint8[] calldata weights
+        uint8[] memory weights
     ) external view returns (uint96 votes) {
         if (weights.length >= 7) {
             revert VaultLimitExceed();
         }
-        uint256 sum;
+        if (weights.length == 0) {
+            weights = defaultWeights;
+        }
+        uint8 sum;
         for (uint8 i; i < weights.length; ) {
             sum += weights[i];
             unchecked {
@@ -97,7 +132,7 @@ contract VaultAggregator is PausableUpgradeable, AccessControlledV8 {
         for (uint8 i; i < weights.length; ) {
             if (weights[i] != 0) {
                 uint96 vote = vaults[i].getPriorVotes(account, blockNumberOrTimestamp);
-                votes = votes + uint96((weights[i] * vote) / sum);
+                votes = votes + ((weights[i] * vote) / sum);
             }
             unchecked {
                 ++i;
--- tests/MultiTokenGovernance/MultiTokenGovernance.ts
+++ tests/MultiTokenGovernance/MultiTokenGovernance.ts
@@ -24,6 +24,7 @@ enum ProposalType {
 describe("MultiTokenGovernance", async () => {
   let deployer: SignerWithAddress;
   let signer1: SignerWithAddress;
+  let signer2: SignerWithAddress;
   let accessControl: AccessControlManager;
   let vaultAggregator: VaultAggregator;
   let governorBravoDelegate;
@@ -36,9 +37,15 @@ describe("MultiTokenGovernance", async () => {
   let fasttrackTimelock: Timelock;
   let token1Amount: BigNumber;
   let token2Amount: BigNumber;
+  let targets: any[];
+  let values: any[];
+  let signatures: any[];
+  let callDatas: any[];
+  let token1Weight: BigNumber;
+  let token2Weight: BigNumber;
 
   const GovernanceFixture = async () => {
-    [deployer, signer1] = await ethers.getSigners();
+    [deployer, signer1, signer2] = await ethers.getSigners();
     const TimelockFactory = await ethers.getContractFactory("Timelock");
     normalTimelock = await TimelockFactory.deploy(deployer.address, 10800);
     fasttrackTimelock = await TimelockFactory.deploy(deployer.address, 7200);
@@ -88,15 +95,27 @@ describe("MultiTokenGovernance", async () => {
     vaultAggregator = await upgrades.deployProxy(vaultAggregatorFactory, [accessControl.address]);
 
     // Give permission
-    const tx = await accessControl.giveCallPermission(
+    let tx = await accessControl.giveCallPermission(
       vaultAggregator.address,
       "updateVault(address,bool)",
       deployer.address,
     );
     await tx.wait();
 
+    tx = await accessControl.giveCallPermission(
+      vaultAggregator.address,
+      "updateDefaultWeights(uint8[])",
+      deployer.address,
+    );
+    await tx.wait();
+
     expect(await vaultAggregator.updateVault(tokenVault1.address, true)).to.emit(vaultAggregator, "UpdateVault");
     expect(await vaultAggregator.updateVault(tokenVault2.address, true)).to.emit(vaultAggregator, "UpdateVault");
+
+    await expect(vaultAggregator.updateDefaultWeights([100, 0, 0, 0, 0, 0, 0, 0])).to.emit(
+      vaultAggregator,
+      "DefaultWeightsUpdated",
+    );
 
     await governorBravoDelegate.setVariable("admin", await deployer.getAddress());
     await governorBravoDelegate.initialize(
@@ -109,17 +128,32 @@ describe("MultiTokenGovernance", async () => {
       [normalTimelock.address, fasttrackTimelock.address, criticalTimelock.address],
       deployer.address,
     );
+    targets = [deployer.address];
+    values = ["0"];
+    signatures = ["getBalanceOf(address)"];
+    callDatas = [ethers.utils.defaultAbiCoder.encode(["address"], [deployer.address])];
+    token1Weight = BigNumber.from(80);
+    token2Weight = BigNumber.from(20);
   };
   beforeEach("Configure Vault", async () => {
     await loadFixture(GovernanceFixture);
   });
-  it("Propose & Voting", async () => {
-    const targets = [deployer.address];
-    const values = ["0"];
-    const signatures = ["getBalanceOf(address)"];
-    const callDatas = [ethers.utils.defaultAbiCoder.encode(["address"], [deployer.address])];
-    const token1Weight = BigNumber.from(80);
-    const token2Weight = BigNumber.from(20);
+  it("Propose successfully", async () => {
+    await expect(
+      governorBravoDelegate.propose(targets, values, signatures, callDatas, "", ProposalType.NORMAL, [
+        token1Weight,
+        token2Weight,
+      ]),
+    ).to.emit(governorBravoDelegate, "ProposalCreated");
+  });
+  it("Revert if proposer does not have enough votes", async () => {
+    await expect(
+      governorBravoDelegate
+        .connect(signer2)
+        .propose(targets, values, signatures, callDatas, "", ProposalType.NORMAL, [token1Weight, token2Weight]),
+    ).to.be.revertedWith("proposer votes below proposal threshold");
+  });
+  it("Vote successfully", async () => {
     await expect(
       governorBravoDelegate.propose(targets, values, signatures, callDatas, "", ProposalType.NORMAL, [
         token1Weight,
@@ -136,6 +170,39 @@ describe("MultiTokenGovernance", async () => {
       .mul(token1Amount)
       .add(token2Weight.mul(token2Amount))
       .div(token1Weight.add(token2Weight));
+
+    const proposal = await governorBravoDelegate.proposals(proposalId);
+    expect(proposal.forVotes).equals(expectedVotes);
+  });
+  it("No votes will count if given weights and default weights are zero", async () => {
+    await expect(vaultAggregator.updateDefaultWeights([0, 0, 0, 0, 0, 0, 0, 0])).to.emit(
+      vaultAggregator,
+      "DefaultWeightsUpdated",
+    );
+    await expect(
+      governorBravoDelegate.propose(targets, values, signatures, callDatas, "", ProposalType.NORMAL, []),
+    ).to.emit(governorBravoDelegate, "ProposalCreated");
+    const proposalId = await governorBravoDelegate.latestProposalIds(deployer.address);
+    await mine();
+    await expect(governorBravoDelegate.connect(signer1).castVote(proposalId, 1)).to.emit(
+      governorBravoDelegate,
+      "VoteCast",
+    );
+    const proposal = await governorBravoDelegate.proposals(proposalId);
+    expect(proposal.forVotes).equals(0);
+  });
+
+  it("Default votes will be counted if weights are not given", async () => {
+    await expect(
+      governorBravoDelegate.propose(targets, values, signatures, callDatas, "", ProposalType.NORMAL, []),
+    ).to.emit(governorBravoDelegate, "ProposalCreated");
+    const proposalId = await governorBravoDelegate.latestProposalIds(deployer.address);
+    await mine();
+    await expect(governorBravoDelegate.connect(signer1).castVote(proposalId, 1)).to.emit(
+      governorBravoDelegate,
+      "VoteCast",
+    );
+    const expectedVotes = BigNumber.from(100).mul(token1Amount).div(100);
 
     const proposal = await governorBravoDelegate.proposals(proposalId);
     expect(proposal.forVotes).equals(expectedVotes);
