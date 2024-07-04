cd8ed361fb1730743475a089172f449f48682e3c
Debugger022 <debugger022@gmail.com>
2024-07-04 19:58:43 +0530

    chore: add config and deployment of ACM on opsepolia

--- helpers/deploy/constants.ts
+++ helpers/deploy/constants.ts
@@ -7,6 +7,8 @@ export enum SUPPORTED_NETWORKS {
   OPBNBMAINNET = "opbnbmainnet",
   ARBITRUM_SEPOLIA = "arbitrumsepolia",
   ARBITRUM_ONE = "arbitrumone",
+  OPSEPOLIA = "opsepolia",
+  OPMAINNET = "opmainnet",
   HARDHAT = "hardhat",
 }
 
@@ -15,10 +17,12 @@ export const LZ_ENDPOINTS: Record<SUPPORTED_NETWORKS, string> = {
   bscmainnet: "0x3c2269811836af69497E5F486A85D7316753cf62",
   opbnbmainnet: "0xb6319cC6c8c27A8F5dAF0dD3DF91EA35C4720dd7",
   arbitrumone: "0x3c2269811836af69497E5F486A85D7316753cf62",
+  opmainnet: "0x3c2269811836af69497E5F486A85D7316753cf62",
   sepolia: "0xae92d5aD7583AD66E49A0c67BAd18F6ba52dDDc1",
   bsctestnet: "0x6Fcb97553D41516Cb228ac03FdC8B9a0a9df04A1",
   opbnbtestnet: "0x83c73Da98cf733B03315aFa8758834b36a195b87",
   arbitrumsepolia: "0x6098e96a28E02f27B1e6BD381f870F1C8Bd169d3",
+  opsepolia: "0x55370E0fBB5f5b8dAeD978BA1c075a499eB107B8",
   hardhat: "0x6Fcb97553D41516Cb228ac03FdC8B9a0a9df04A1",
 };
 
@@ -27,10 +31,12 @@ export const LZ_CHAINID: Record<SUPPORTED_NETWORKS, number> = {
   bscmainnet: 102,
   opbnbmainnet: 202,
   arbitrumone: 110,
+  opmainnet: 111,
   sepolia: 10161,
   bsctestnet: 10102,
   opbnbtestnet: 10202,
   arbitrumsepolia: 10231,
+  opsepolia: 10232,
   hardhat: 10102,
 };
 
--- helpers/deploy/deploymentUtils.ts
+++ helpers/deploy/deploymentUtils.ts
@@ -18,12 +18,15 @@ export const getAcmAdminAccount = async (network: SUPPORTED_NETWORKS): Promise<s
   } else if (network === "opbnbtestnet") {
     return "0xb15f6EfEbC276A3b9805df81b5FB3D50C2A62BDf"; // OPBNBTESTNET MULTISIG
   } else if (network === "opbnbmainnet") {
-    return "0xC46796a21a3A9FAB6546aF3434F2eBfFd0604207"; //OPBNBMAINNET MULTISIG
+    return "0xC46796a21a3A9FAB6546aF3434F2eBfFd0604207"; // OPBNBMAINNET MULTISIG
   } else if (network === "arbitrumsepolia") {
-    return "0x1426A5Ae009c4443188DA8793751024E358A61C2"; //ARBITRUM SEPOLIA MULTISIG
+    return "0x1426A5Ae009c4443188DA8793751024E358A61C2"; // ARBITRUM SEPOLIA MULTISIG
   } else if (network === "arbitrumone") {
-    return "0x14e0E151b33f9802b3e75b621c1457afc44DcAA0"; //ARBITRUM ONE MULTISIG
+    return "0x14e0E151b33f9802b3e75b621c1457afc44DcAA0"; // ARBITRUM ONE MULTISIG
+  } else if (network === "opsepolia") {
+    return "0xd57365EE4E850e881229e2F8Aa405822f289e78d"; // OPSEPOLIA MULTISIG
   }
+
   const normalTimelock = await ethers.getContract("NormalTimelock");
   return normalTimelock.address;
 };
@@ -44,7 +47,10 @@ export const guardian = async (network: SUPPORTED_NETWORKS): Promise<string> => 
     return "0x1426A5Ae009c4443188DA8793751024E358A61C2"; //ARBITRUM SEPOLIA MULTISIG
   } else if (network === "arbitrumone") {
     return "0x14e0E151b33f9802b3e75b621c1457afc44DcAA0"; //ARBITRUM ONE MULTISIG
+  } else if (network === "opsepolia") {
+    return "0xd57365EE4E850e881229e2F8Aa405822f289e78d"; // OPSEPOLIA MULTISIG
   }
+
   return deployer;
 };
 
