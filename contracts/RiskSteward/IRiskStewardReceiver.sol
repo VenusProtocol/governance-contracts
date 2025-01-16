690f1b5137e6ff94ca0ffb00671068ca3f36cdcb
Corey Rice <coreyarice@gmail.com>
2025-01-16 15:15:55 -0300

    refactor: break out update logic to specific contract

--- contracts/RiskSteward/IRiskStewardReceiver.sol
+++ contracts/RiskSteward/IRiskStewardReceiver.sol
@@ -1,28 +1,19 @@
 // SPDX-License-Identifier: BSD-3-Clause
 pragma solidity 0.8.25;
 import { IRiskOracle } from "../interfaces/IRiskOracle.sol";
-import { ICorePoolComptroller } from "../interfaces/ICorePoolComptroller.sol";
 
 struct RiskParamConfig {
     bool active;
     uint256 debounce;
-    uint256 maxIncreaseBps;
-    bool isRelative;
+    address riskSteward;
 }
 
 interface IRiskStewardReceiver {
     function RISK_ORACLE() external view returns (IRiskOracle);
 
-    function CORE_POOL_COMPTROLLER() external view returns (ICorePoolComptroller);
+    function initialize(address accessControlManager_) external;
 
-    function initialize() external;
-
-    function setRiskParameterConfig(
-        string calldata updateType,
-        uint256 debounce,
-        uint256 maxIncreaseBps,
-        bool isRelative
-    ) external;
+    function setRiskParameterConfig(string calldata updateType, address riskSteward, uint256 debounce) external;
 
     function getRiskParameterConfig(string calldata updateType) external view returns (RiskParamConfig memory);
 
