5e26fe8738f25ff8a909c8e0e5ca595d4ed88081
Corey Rice <coreyarice@gmail.com>
2025-01-21 13:18:12 -0300

    feat: add RiskSteward deployments

--- deploy/014-risk-stewards.ts
+++ deploy/014-risk-stewards.ts
@@ -1,8 +1,23 @@
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
-import { SUPPORTED_NETWORKS } from "../helpers/deploy/constants";
+import { LZ_CHAINID, SUPPORTED_NETWORKS } from "../helpers/deploy/constants";
 import { getRiskOracle, getAcmAdminAccount, skipRemoteNetworks } from "../helpers/deploy/deploymentUtils";
+
+const marketCapsRiskStewardNetworkConfigs = {
+  hardhat: {
+    maxDeltaBps: 5000,
+    debouncePeriod: 5,
+  },
+  bsctestnet: {
+    maxDeltaBps: 5000,
+    debouncePeriod: 5,
+  },
+  bscmainnet: {
+    maxDeltaBps: 5000,
+    debouncePeriod: 5,
+  },
+};
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts } = hre;
@@ -11,9 +26,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
   const networkName = hre.network.name as "bsctestnet" | "bscmainnet" | "hardhat";
 
-  const accessControlManager = await hre.ethers.getContract("AccessControlManager");
-  const corePoolComptroller = await hre.ethers.getContract("Unitroller");
+  const marketCapsRiskStewardConfig = marketCapsRiskStewardNetworkConfigs[networkName];
 
+  const accessControlManager = await hre.ethers.getContract("AccessControlManager");
+
+  const governorBravo = await hre.ethers.getContract("GovernorBravoDelegator");
+  const omnichainProposalSender = await hre.ethers.getContract("OmnichainProposalSender");
+  const layerZeroChainId = LZ_CHAINID[networkName];
   // Explicitly mentioning Default Proxy Admin contract path to fetch it from hardhat-deploy instead of OpenZeppelin
   // as zksync doesnot compile OpenZeppelin contracts using zksolc. It is backward compatible for all networks as well.
   const defaultProxyAdmin = await hre.artifacts.readArtifact(
@@ -24,7 +43,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     from: deployer,
     log: true,
     deterministicDeployment: false,
-    args: [await getRiskOracle(networkName)],
+    args: [await getRiskOracle(networkName), governorBravo.address, omnichainProposalSender.address, layerZeroChainId],
     proxy: {
       owner: networkName === "hardhat" ? deployer : await getAcmAdminAccount(networkName as SUPPORTED_NETWORKS),
       proxyContract: "OptimizedTransparentUpgradeableProxy",
@@ -36,6 +55,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
         name: "DefaultProxyAdmin",
         artifact: defaultProxyAdmin,
       },
+      upgradeIndex: 0,
     },
   });
 
@@ -45,18 +65,23 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     from: deployer,
     log: true,
     deterministicDeployment: false,
-    args: [corePoolComptroller.address, riskStewardReceiver.address],
+    args: [riskStewardReceiver.address, layerZeroChainId],
     proxy: {
       owner: networkName === "hardhat" ? deployer : await getAcmAdminAccount(networkName as SUPPORTED_NETWORKS),
       proxyContract: "OptimizedTransparentUpgradeableProxy",
       execute: {
         methodName: "initialize",
-        args: [accessControlManager.address, 5000],
+        args: [
+          accessControlManager.address,
+          marketCapsRiskStewardConfig.maxDeltaBps,
+          marketCapsRiskStewardConfig.debouncePeriod,
+        ],
       },
       viaAdminContract: {
         name: "DefaultProxyAdmin",
         artifact: defaultProxyAdmin,
       },
+      upgradeIndex: 0,
     },
   });
 };
