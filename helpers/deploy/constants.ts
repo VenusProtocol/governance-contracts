861990916bbd28b58882d2de9eeae3a8a5f23701
Corey Rice <coreyarice@gmail.com>
2025-02-24 11:17:19 -0300

    fix: typo

--- helpers/deploy/constants.ts
+++ helpers/deploy/constants.ts
@@ -2,7 +2,7 @@ export enum SUPPORTED_NETWORKS {
   BSCTESTNET = "bsctestnet",
   BSCMAINNET = "bscmainnet",
   SEPOLIA = "sepolia",
-  ETHERUEM = "ethereum",
+  ETHEREUM = "ethereum",
   OPBNBTESTNET = "opbnbtestnet",
   OPBNBMAINNET = "opbnbmainnet",
   ARBITRUM_SEPOLIA = "arbitrumsepolia",
