657fdd4b0039635b2838f8bf0a4b885ce2e86112
Corey Rice <coreyarice@gmail.com>
2025-03-07 10:35:25 -0300

    feat: add testnet deployments

--- deploy/011-risk-stewards.ts
+++ deploy/011-risk-stewards.ts
@@ -2,17 +2,19 @@ import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
 import { SUPPORTED_NETWORKS } from "../helpers/deploy/constants";
-import { getRiskOracle, guardian, skipRemoteNetworks } from "../helpers/deploy/deploymentUtils";
+import { getRiskOracle } from "../helpers/deploy/deploymentUtils";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts } = hre;
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
 
-  const networkName = hre.network.name as "bsctestnet" | "bscmainnet" | "hardhat";
+  const networkName = hre.network.name as SUPPORTED_NETWORKS;
 
   const accessControlManager = await hre.ethers.getContract("AccessControlManager");
-  const corePoolComptroller = await hre.ethers.getContract("Unitroller");
+  const corePoolComptroller = (await hre.ethers.getContractOrNull("Unitroller")) || {
+    address: "0x0000000000000000000000000000000000000001",
+  };
 
   // Explicitly mentioning Default Proxy Admin contract path to fetch it from hardhat-deploy instead of OpenZeppelin
   // as zksync doesnot compile OpenZeppelin contracts using zksolc. It is backward compatible for all networks as well.
@@ -20,13 +22,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     "hardhat-deploy/solc_0.8/openzeppelin/proxy/transparent/ProxyAdmin.sol:ProxyAdmin",
   );
 
+  const normalTimelockAddress = (await hre.ethers.getContract("NormalTimelock")).address;
   await deploy("RiskStewardReceiver", {
     from: deployer,
     log: true,
-    deterministicDeployment: false,
     args: [await getRiskOracle(networkName)],
     proxy: {
-      owner: networkName === "hardhat" ? deployer : await guardian(networkName as SUPPORTED_NETWORKS),
+      owner: networkName === "hardhat" ? deployer : normalTimelockAddress,
       proxyContract: "OptimizedTransparentUpgradeableProxy",
       execute: {
         methodName: "initialize",
@@ -41,13 +43,16 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
   const riskStewardReceiver = await hre.ethers.getContract("RiskStewardReceiver");
 
+  if ((await riskStewardReceiver.owner()) === deployer) {
+    await riskStewardReceiver.transferOwnership(normalTimelockAddress);
+  }
+
   await deploy("MarketCapsRiskSteward", {
     from: deployer,
     log: true,
-    deterministicDeployment: false,
     args: [corePoolComptroller.address, riskStewardReceiver.address],
     proxy: {
-      owner: networkName === "hardhat" ? deployer : await guardian(networkName as SUPPORTED_NETWORKS),
+      owner: networkName === "hardhat" ? deployer : normalTimelockAddress,
       proxyContract: "OptimizedTransparentUpgradeableProxy",
       execute: {
         methodName: "initialize",
@@ -59,10 +64,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
       },
     },
   });
+
+  const marketCapsRiskSteward = await hre.ethers.getContract("MarketCapsRiskSteward");
+  if ((await marketCapsRiskSteward.owner()) === deployer) {
+    await marketCapsRiskSteward.transferOwnership(normalTimelockAddress);
+  }
 };
 
 func.tags = ["risk-stewards"];
-
-func.skip = skipRemoteNetworks();
 
 export default func;
