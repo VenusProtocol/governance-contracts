61c48b1c137ab02bd8b43c6eb6bba2e8af2396c6
web3rover <arsenal.narayan@gmail.com>
2025-04-03 18:21:08 +0530

    fix: deployed on testnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -96,11 +96,8 @@ const config: HardhatUserConfig = {
       url: process.env.ARCHIVE_NODE_sepolia || "https://ethereum-sepolia.blockpi.network/v1/rpc/public",
       chainId: 11155111,
       live: true,
-      gasPrice: 20000000000, // 20 gwei
-      // accounts: DEPLOYER_PRIVATE_KEY ? [`0x${DEPLOYER_PRIVATE_KEY}`] : [],
-      accounts: {
-        mnemonic: process.env.MNEMONIC || "",
-      },
+      gasPrice: 25000000000, // 25 gwei
+      accounts: DEPLOYER_PRIVATE_KEY ? [`0x${DEPLOYER_PRIVATE_KEY}`] : [],
     },
     ethereum: {
       url: process.env.ARCHIVE_NODE_ethereum || "https://ethereum.blockpi.network/v1/rpc/public",
