bab311e6d9706c798fb8cbd1a088decd426a9af3
GitGuru7 <aditya@venus.io>
2025-04-11 16:09:21 +0530

    feat: add deployment script for bravo delegate

---
+++ deploy/011-deploy-governer-bravo.ts
@@ -0,0 +1,32 @@
+import { ethers } from "hardhat";
+import { DeployFunction } from "hardhat-deploy/types";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts } = hre;
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  await deploy("GovernorBravoDelegate", {
+    from: deployer,
+    args: [],
+    log: true,
+    autoMine: true,
+    skipIfAlreadyDeployed: false,
+  });
+
+  const GovernorBravoDelegate = await ethers.getContract("GovernorBravoDelegate");
+
+  const GovernorBravoDelegator = await ethers.getContract("GovernorBravoDelegator");
+  if (GovernorBravoDelegator.admin() == deployer) {
+    const tx = await GovernorBravoDelegator._setImplementation(GovernorBravoDelegate.address);
+    await tx.wait();
+  }
+};
+
+func.skip = async (hre: HardhatRuntimeEnvironment) =>
+  !(hre.network.name === "bsctestnet" || hre.network.name === "bscmainnet") && hre.network.name !== "hardhat";
+
+func.tags = ["governor-bravo"];
+
+export default func;
